a4b0a8fc502da41ce18fad7a53da9175d23afe24
paluszekNN <adampaluch95@gmail.com>
2019-08-02 10:54:53 +0200

    change some variables to const and play game with load model

--- play_checkers.py
+++ play_checkers.py
@@ -1,31 +1,57 @@
-from game import Checkers
-import random
-import numpy as np
 from min_max import min_max_player
-
-
-def random_play(game):
-    game.available_moves()
-    moves = game.moves
-    return random.choice(moves)
+from ai import *
+import tensorflow as tf
+import min_max
 
 
 if __name__ == '__main__':
-    game = Checkers()
-    n = 0
-    while True:
-        n += 1
-        game.reset()
-        while not game.win:
-            if game.turn == 1:
-                game.move(random_play(game))
-            else:
+    model = DQN(INPUT_SIZE, ACTION_SPACE_SIZE, CONV_LAYER_SIZE, HIDDEN_LAYER_SIZE, 'model')
+    with tf.Session() as session:
+        model.set_session(session)
+
+        model.load('tf_dqn_weights1.npz')
+
+        experience_replay_buffer = ReplayMemory(INPUT_SIZE)
+
+        for i in range(50):
+            GAME.available_moves()
+            if GAME.win != 0:
+                GAME.reset()
+            move = random_play()
+            action = encoding_move(move)
+            GAME.move(move)
+            if GAME.win == 0:
                 new_game = Checkers()
-                new_game.board_state = np.array(game.board_state)
-                new_game.turn = game.turn
-                new_game.moves_queen_with_out_capture = game.moves_queen_with_out_capture
-                move = min_max_player(new_game, new_game.turn)
-                game.move(move)
-            game.print_board()
-        if n % 100 == 0:
-            print(n)
+                new_game.board_state = np.array(GAME.board_state)
+                new_game.turn = GAME.turn
+                new_game.moves_queen_with_out_capture = GAME.moves_queen_with_out_capture
+                move = min_max.min_max_player(new_game, new_game.turn)
+                GAME.move(move)
+            reward = GAME.win
+            experience_replay_buffer.add_experince(action, GAME.board_state, reward)
+
+        n = 0
+        while True:
+            n += 1
+            GAME.reset()
+            while not GAME.win:
+                if GAME.turn == 1:
+                    GAME.available_moves()
+                    states, _, _, _, _ = experience_replay_buffer.get_minibatch()
+                    action = model.sample_action(states, eps=0.0001)
+                    GAME.move(decoding_move(action))
+                    # GAME.move(random_play(game))
+                else:
+                    new_game = Checkers()
+                    new_game.board_state = np.array(GAME.board_state)
+                    new_game.turn = GAME.turn
+                    new_game.moves_queen_with_out_capture = GAME.moves_queen_with_out_capture
+                    move = min_max_player(new_game, new_game.turn)
+                    GAME.move(move)
+
+                reward = GAME.win
+
+                experience_replay_buffer.add_experince(action, GAME.board_state, reward)
+                GAME.print_board()
+            if n % 100 == 0:
+                print(n)
